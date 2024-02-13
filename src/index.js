cae42460e826d6fba1b3a59d9885cbe1f188e5fc
Kimberly Alcock <kimberly.a.alcock@gmail.com>
2024-02-13 13:21:29 -0500

    Initial upload

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function openTab(tabName) {
+    let i, tabs;
+    tabs = document.getElementsByClassName("tab");
+    for (i = 0; i < tabs.length; i++) {
+        tabs[i].classList.remove("active");
+    }
+    document.getElementById(tabName).classList.add("active");
+}
