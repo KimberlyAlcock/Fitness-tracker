01185dcffc9d73b024933940f5efc340d06164c0
Kimberly Alcock <kimberly.a.alcock@gmail.com>
2024-02-27 09:42:10 -0500

    update planks.js file to save user inputted data in an object

--- static/src/planks.js
+++ static/src/planks.js
@@ -1,3 +1,4 @@
+// Sets event listener to get info from user
 document.getElementById("submitBtn").addEventListener("click", function() {
     // Get user input values
     let date = document.getElementById("tracker-date").querySelector("input").value;
@@ -7,6 +8,15 @@ document.getElementById("submitBtn").addEventListener("click", function() {
 
     // Create a new table row
     let newRow = document.createElement("tr");
+
+    let userInput = {
+        Date: date,
+        Planks: planks,
+        Pullups: pullups,
+        "Double-unders": doubleUnders
+    };
+    
+    console.log(userInput)
 
     // Insert data into the new row
     newRow.innerHTML = `
@@ -18,8 +28,15 @@ document.getElementById("submitBtn").addEventListener("click", function() {
 
     // Append the new row to the table body
     document.getElementById("data-body").appendChild(newRow);
+
+    // Clear the form inputs
+    document.getElementById("tracker-date").querySelector("input").value = "";
+    document.getElementById("planks-input").querySelector("input").checked = false;
+    document.getElementById("pullups-input").querySelector("input").checked = false;
+    document.getElementById("double-unders-input").querySelector("input").checked = false;
 });
 
+// Sets event listener for Reset button
 document.getElementById("resetBtn").addEventListener("click", function() {
     // Clear the form inputs
     document.getElementById("tracker-date").querySelector("input").value = "";
@@ -27,3 +44,8 @@ document.getElementById("resetBtn").addEventListener("click", function() {
     document.getElementById("pullups-input").querySelector("input").checked = false;
     document.getElementById("double-unders-input").querySelector("input").checked = false;
 });
+
+
+
+
+
